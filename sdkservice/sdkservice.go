60b55e95c14130cb1aaf750b72f5e632ecd1e69b
Gianguido Sorà <me@gsora.xyz>
2021-12-02 12:12:40 +0100

    feature: basic implementation of sdk-service connector

---
+++ sdkservice/sdkservice.go
@@ -0,0 +1,41 @@
+package sdkservice
+
+import (
+	//sdkservicetypes "github.com/allinbits/sdk-service-meta"
+
+	"fmt"
+
+	sdkserviceclient "github.com/allinbits/sdk-service-meta/gen/grpc/sdk_utilities/client"
+	sdkutilities "github.com/allinbits/sdk-service-meta/gen/sdk_utilities"
+	"google.golang.org/grpc"
+)
+
+const sdkServiceURLFmt = "sdk-service-v%s"
+
+func sdkServiceURL(version string) string {
+	return fmt.Sprintf(sdkServiceURLFmt, version)
+}
+
+func Client(sdkVersion string) (sdkutilities.Client, error) {
+	conn, err := grpc.Dial(sdkServiceURL(sdkVersion), grpc.WithInsecure())
+	if err != nil {
+		return sdkutilities.Client{}, fmt.Errorf("cannot connect to endpoint %s: %w", sdkServiceURL(sdkVersion), err)
+	}
+
+	client := sdkserviceclient.NewClient(conn)
+
+	cc := sdkutilities.Client{
+		SupplyEndpoint:              client.Supply(),
+		QueryTxEndpoint:             client.QueryTx(),
+		BroadcastTxEndpoint:         client.BroadcastTx(),
+		TxMetadataEndpoint:          client.TxMetadata(),
+		BlockEndpoint:               client.Block(),
+		LiquidityParamsEndpoint:     client.LiquidityParams(),
+		LiquidityPoolsEndpoint:      client.LiquidityPools(),
+		MintInflationEndpoint:       client.MintInflation(),
+		MintParamsEndpoint:          client.MintParams(),
+		MintAnnualProvisionEndpoint: client.MintAnnualProvision(),
+	}
+
+	return cc, nil
+}
