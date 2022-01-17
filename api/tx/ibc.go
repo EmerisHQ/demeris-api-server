a66287da3a4e460482b1bd359695f75e8ba128f0
sahithn <sahithnarahari@gmail.com>
2022-01-17 19:36:15 +0530

    update error handling

--- api/tx/ibc.go
+++ api/tx/ibc.go
@@ -11,17 +11,18 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-// GetDestTx returns the transaction status n.
-// @Summary Gets ticket by id.
-// @Tags Chain
-// @ID txTicket
-// @Description Gets transaction status by ticket id.
-// @Param ticketId path string true "ticket id"
-// @Param chainName path string true "chain name"
+// GetDestTx returns tx hash on destination chain.
+// @Summary Gets tx hash on destination chain.
+// @Tags Tx
+// @ID destTx
+// @Description Gets tx hash on destination chain.
+// @Param srcChain path string true "source chain name"
+// @Param destChainName path string true "destination chain name"
+// @Param txHash path string true "tx hash on src chain"
 // @Produce json
-// @Success 200 {object} store.Ticket
+// @Success 200 {object} DestTxResponse
 // @Failure 500,403 {object} deps.Error
-// @Router /tx/ticket/{chainName}/{ticketId} [get]
+// @Router /tx/{srcChain}/{destChain}/{txHash} [get]
 func GetDestTx(c *gin.Context) {
 	d := deps.GetDeps(c)
 
@@ -30,33 +31,96 @@ func GetDestTx(c *gin.Context) {
 	txHash := c.Param("txHash")
 
 	url := fmt.Sprintf("http://%s:26657/tx?hash=%s&prove=%t", srcChain, "0x"+txHash, false)
+
+	bz, err := GetUrlRes(url)
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve tx info of %s on %s", txHash, srcChain),
+			http.StatusBadRequest,
+		)
+
+		d.WriteError(c, e,
+			"cannot retrieve tx info",
+			"id",
+			e.ID,
+			"txHash",
+			txHash,
+			"src chain name",
+			srcChain,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	r := gjson.GetBytes(bz, "result.tx_result.events.3.attributes.3.value")
+	seq, err := base64.StdEncoding.DecodeString(r.String())
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve packet sequenceof %s on %s", txHash, srcChain),
+			http.StatusBadRequest,
+		)
+
+		d.WriteError(c, e,
+			"cannot retrieve sequence",
+			"id",
+			e.ID,
+			"txHash",
+			txHash,
+			"src chain name",
+			srcChain,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	url = fmt.Sprintf("http://%s:26657/tx_search?query=\"recv_packet.packet_sequence=%s\"", destChain, string(seq))
+
+	bz, err = GetUrlRes(url)
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve tx with packet sequence %d on %s", seq, destChain),
+			http.StatusBadRequest,
+		)
+
+		d.WriteError(c, e,
+			"cannot retrieve destination tx",
+			"id",
+			e.ID,
+			"txHash",
+			txHash,
+			"dest chain name",
+			destChain,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	r = gjson.GetBytes(bz, "result.txs.0.hash")
+	c.JSON(http.StatusOK, DestTxResponse{
+		DestChain: destChain,
+		TxHash:    r.String(),
+	})
+}
+
+func GetUrlRes(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		d.LogError("http get", err)
-		return
+		return nil, err
 	}
 
 	bz, err := io.ReadAll(resp.Body)
 	if err != nil {
-	}
-	r := gjson.GetBytes(bz, "result.tx_result.events.3.attributes.3.value")
-	d.Logger.Debugw("thi si tx", "tx", r)
-
-	seq, err := base64.StdEncoding.DecodeString(r.String())
-
-	url = fmt.Sprintf("http://%s:26657/tx_search?query=\"recv_packet.packet_sequence=%s\"", destChain, string(seq))
-	d.Logger.Debugw("this log", "url", url)
-	resp, err = http.Get(url)
-	if err != nil {
-		d.LogError("http get", err)
-		return
-	}
-	bz, err = io.ReadAll(resp.Body)
-	if err != nil {
+		return nil, err
 	}
 
-	r = gjson.GetBytes(bz, "result.txs.0.hash")
-	d.Logger.Debugw("thi si tx", "tx", r)
-
-	c.JSON(http.StatusOK, r.String())
+	return bz, nil
 }
