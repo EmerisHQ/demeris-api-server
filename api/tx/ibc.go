ae3d1bb4752e7eb5040b8e4df6ae6757d800fefe
sahithn <sahithnarahari@gmail.com>
2022-01-18 18:31:17 +0530

    use sdk service

--- api/tx/ibc.go
+++ api/tx/ibc.go
@@ -1,12 +1,14 @@
 package tx
 
 import (
-	"encoding/base64"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/allinbits/demeris-api-server/api/router/deps"
+	"github.com/allinbits/demeris-api-server/sdkservice"
+	sdkutilities "github.com/allinbits/sdk-service-meta/gen/sdk_utilities"
 	"github.com/gin-gonic/gin"
 	"github.com/tidwall/gjson"
 )
@@ -30,62 +32,108 @@ func GetDestTx(c *gin.Context) {
 	destChain := c.Param("destChain")
 	txHash := c.Param("txHash")
 
-	url := fmt.Sprintf("http://%s:26657/tx?hash=%s&prove=%t", srcChain, "0x"+txHash, false)
+	//url := fmt.Sprintf("http://%s:26657/tx?hash=%s&prove=%t", srcChain, "0x"+txHash, false)
+	//
+	//bz, err := GetUrlRes(url)
+	//if err != nil {
+	//	e := deps.NewError(
+	//		"chains",
+	//		fmt.Errorf("cannot retrieve tx info of %s on %s", txHash, srcChain),
+	//		http.StatusBadRequest,
+	//	)
+	//
+	//	d.WriteError(c, e,
+	//		"cannot retrieve tx info",
+	//		"id",
+	//		e.ID,
+	//		"txHash",
+	//		txHash,
+	//		"src chain name",
+	//		srcChain,
+	//		"error",
+	//		err,
+	//	)
+	//
+	//	return
+	//}
+
+	chain, err := d.Database.Chain(srcChain)
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve chain with name %v", srcChain),
+			http.StatusBadRequest,
+		)
+
+		d.WriteError(c, e,
+			"cannot retrieve chain",
+			"id",
+			e.ID,
+			"name",
+			srcChain,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	client, err := sdkservice.Client(chain.MajorSDKVersion())
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve sdk-service for version %s with chain name %v", chain.CosmosSDKVersion, chain.ChainName),
+			http.StatusBadRequest,
+		)
+
+		d.WriteError(c, e,
+			"cannot retrieve chain's sdk-service",
+			"id",
+			e.ID,
+			"name",
+			srcChain,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	sdkRes, err := client.QueryTx(context.Background(), &sdkutilities.QueryTxPayload{
+		ChainName: srcChain,
+		Hash:      txHash,
+	})
+
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve tx from sdk-service, %w", err),
+			http.StatusBadRequest,
+		)
+
+		d.WriteError(c, e,
+			"cannot retrieve tx from sdk-service",
+			"id",
+			e.ID,
+			"txHash",
+			txHash,
+			"src chain name",
+			srcChain,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	r := gjson.GetBytes(sdkRes, "tx_response.logs.0.events.2.attributes.3.value")
+	url := fmt.Sprintf("http://%s:26657/tx_search?query=\"recv_packet.packet_sequence=%s\"", destChain, r.String())
 
 	bz, err := GetUrlRes(url)
 	if err != nil {
 		e := deps.NewError(
 			"chains",
-			fmt.Errorf("cannot retrieve tx info of %s on %s", txHash, srcChain),
-			http.StatusBadRequest,
-		)
-
-		d.WriteError(c, e,
-			"cannot retrieve tx info",
-			"id",
-			e.ID,
-			"txHash",
-			txHash,
-			"src chain name",
-			srcChain,
-			"error",
-			err,
-		)
-
-		return
-	}
-
-	r := gjson.GetBytes(bz, "result.tx_result.events.3.attributes.3.value")
-	seq, err := base64.StdEncoding.DecodeString(r.String())
-	if err != nil {
-		e := deps.NewError(
-			"chains",
-			fmt.Errorf("cannot retrieve packet sequenceof %s on %s", txHash, srcChain),
-			http.StatusBadRequest,
-		)
-
-		d.WriteError(c, e,
-			"cannot retrieve sequence",
-			"id",
-			e.ID,
-			"txHash",
-			txHash,
-			"src chain name",
-			srcChain,
-			"error",
-			err,
-		)
-
-		return
-	}
-
-	url = fmt.Sprintf("http://%s:26657/tx_search?query=\"recv_packet.packet_sequence=%s\"", destChain, string(seq))
-
-	bz, err = GetUrlRes(url)
-	if err != nil {
-		e := deps.NewError(
-			"chains",
-			fmt.Errorf("cannot retrieve tx with packet sequence %d on %s", seq, destChain),
+			fmt.Errorf("cannot retrieve tx with packet sequence %d on %s", r.String(), destChain),
 			http.StatusBadRequest,
 		)
 
