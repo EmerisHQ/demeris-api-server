abe30bc73c71d4162bb6dc20e65e97c21dc84ac9
SpideyPool192 <krishna192reddy@gmail.com>
2022-01-28 14:05:46 +0530

    refactor primary channel test to use testMain

--- api/chains/chains_primaryChannel_test.go
+++ api/chains/chains_primaryChannel_test.go
@@ -6,98 +6,34 @@ import (
 	"io/ioutil"
 	"net/http"
 	"testing"
-	"time"
 
-	"github.com/allinbits/demeris-api-server/api/config"
-	"github.com/allinbits/demeris-api-server/api/database"
-	"github.com/allinbits/demeris-api-server/api/router"
-	"github.com/allinbits/demeris-backend-models/cns"
-	cnsDB "github.com/allinbits/emeris-cns-server/cns/database"
-	"github.com/allinbits/emeris-utils/logging"
-	"github.com/allinbits/emeris-utils/store"
-	"github.com/cockroachdb/cockroach-go/v2/testserver"
-	"github.com/lib/pq"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
 )
 
 func TestGetPrimaryChannelWithCounterparty(t *testing.T) {
-	r, client, instance, cfg, _, tDown := setup(t)
-	defer tDown()
 
-	require.NotNil(t, r)
+	require.NoError(t, testingCtx.CnsDB.AddChain(chainWithPublicEndpoints))
 
-	primaryChannels := cns.DbStringMap{
-		"terra": "channel-1",
-	}
-	newChain := cns.Chain{
-		ChainName:        "akash",
-		Enabled:          true,
-		PrimaryChannel:   primaryChannels,
-		DemerisAddresses: pq.StringArray{},
-		SupportedWallets: pq.StringArray{},
-	}
-
-	require.NoError(t, instance.AddChain(newChain))
-
-	body := getRespBody(t, "/chain/akash/primary_channel/terra", client, cfg.ListenAddr)
+	body, status := getRespBodyAndStatus(t, "/chain/chain2/primary_channel/key", testingCtx.Cfg.ListenAddr)
 
 	actual := getValFromByteArr(t, body, "primary_channel")
 
 	expected := map[string]interface{}{
-		"channel_name": "channel-1",
-		"counterparty": "terra",
+		"channel_name": "value",
+		"counterparty": "key",
 	}
 	require.Equal(t, expected, actual)
+	require.Equal(t, 200, status)
 
-	body = getRespBody(t, "/chain/akash/primary_channel/cosmos-hub", client, cfg.ListenAddr)
+	body, status = getRespBodyAndStatus(t, "/chain/chain2/primary_channel/cosmos-hub", testingCtx.Cfg.ListenAddr)
 	actual = getValFromByteArr(t, body, "cause")
-	require.Equal(t, "cannot retrieve primary channel between akash and cosmos-hub", actual)
+	require.Equal(t, "cannot retrieve primary channel between chain2 and cosmos-hub", actual)
+	require.Equal(t, 400, status)
 
-	body = getRespBody(t, "/chain/osmosis/primary_channel/terra", client, cfg.ListenAddr)
+	body, status = getRespBodyAndStatus(t, "/chain/osmosis/primary_channel/terra", testingCtx.Cfg.ListenAddr)
 	actual = getValFromByteArr(t, body, "cause")
 	require.Equal(t, "cannot retrieve chain with name osmosis", actual)
-}
-
-func setup(t *testing.T) (router.Router, http.Client, cnsDB.Instance, config.Config, zap.SugaredLogger, func()) {
-	t.Helper()
-	ts, err := testserver.NewTestServer()
-	require.NoError(t, err)
-
-	require.NoError(t, ts.WaitForInit())
-
-	connStr := ts.PGURL().String()
-	cnsInstance, err := cnsDB.New(connStr)
-	require.NoError(t, err)
-
-	cfg := &config.Config{
-		DatabaseConnectionURL: connStr,
-		ListenAddr:            "127.0.0.1:9090",
-		RedisAddr:             "127.0.0.1:6379",
-		KubernetesNamespace:   "emeris",
-		Debug:                 true,
-	}
-
-	db, err := database.Init(cfg)
-	require.NoError(t, err)
-
-	s, err := store.NewClient(cfg.RedisAddr)
-	require.NoError(t, err)
-
-	l := logging.New(logging.LoggingConfig{
-		Debug: cfg.Debug,
-	})
-
-	r := router.New(db, l, s, nil, "emeris", nil, cfg.Debug)
-
-	go r.Serve(cfg.ListenAddr)
-	time.Sleep(2 * time.Second)
-
-	client := http.Client{
-		Timeout: 2 * time.Second,
-	}
-
-	return *r, client, *cnsInstance, *cfg, *l, func() { ts.Stop() }
+	require.Equal(t, 400, status)
 }
 
 func getValFromByteArr(t *testing.T, b []byte, key string) interface{} {
@@ -109,16 +45,14 @@ func getValFromByteArr(t *testing.T, b []byte, key string) interface{} {
 	return data[key]
 }
 
-func getRespBody(t *testing.T, endPoint string, client http.Client, listenAddr string) []byte {
-	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s%s", listenAddr, endPoint), nil)
-	require.NoError(t, err)
+func getRespBodyAndStatus(t *testing.T, endPoint string, listenAddr string) ([]byte, int) {
 
-	res, err := client.Do(req)
+	res, err := http.Get(fmt.Sprintf("http://%s%s", listenAddr, endPoint))
 	require.NoError(t, err)
 
 	body, err := ioutil.ReadAll(res.Body)
 	require.NoError(t, err)
 	require.NoError(t, res.Body.Close())
 
-	return body
+	return body, res.StatusCode
 }
