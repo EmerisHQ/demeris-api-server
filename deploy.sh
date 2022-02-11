116f3268fccfaed31a03103b5fc2be73ba507b2a
Antonio Pitasi <antonio@pitasi.dev>
2022-02-11 13:51:37 +0100

    build: add `deploy.sh` for local deployments

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+docker build -t emeris/api-server --build-arg GIT_TOKEN=$GITHUB_TOKEN -f Dockerfile .
+
+kind load docker-image emeris/api-server --name emeris
+
+helm upgrade api-server \
+       --install \
+       --kube-context kind-emeris \
+       --namespace emeris \
+       --set imagePullPolicy=Never \
+       ./helm
