eb12eb856fc421abc2044fec5defd75b6ae42d9f
akhilkumarpilli <akhilkumar7947@gmail.com>
2022-02-11 11:34:28 +0530

    make visible models for CI scripts

--- api/account/models.go
+++ api/account/models.go
@@ -4,57 +4,57 @@ import (
 	"github.com/allinbits/demeris-backend-models/tracelistener"
 )
 
-type balancesResponse struct {
-	Balances []balance `json:"balances"`
+type BalancesResponse struct {
+	Balances []Balance `json:"balances"`
 }
-type balance struct {
+type Balance struct {
 	Address   string  `json:"address,omitempty"`
 	BaseDenom string  `json:"base_denom,omitempty"`
 	Verified  bool    `json:"verified"`
 	Amount    string  `json:"amount,omitempty"`
 	OnChain   string  `json:"on_chain,omitempty"`
-	Ibc       ibcInfo `json:"ibc,omitempty"`
+	Ibc       IbcInfo `json:"ibc,omitempty"`
 }
 
-type ibcInfo struct {
+type IbcInfo struct {
 	Path string `json:"path,omitempty"`
 	Hash string `json:"hash,omitempty"`
 }
 
-type stakingBalancesResponse struct {
-	StakingBalances []stakingBalance `json:"staking_balances"`
+type StakingBalancesResponse struct {
+	StakingBalances []StakingBalance `json:"staking_balances"`
 }
 
-type stakingBalance struct {
+type StakingBalance struct {
 	ValidatorAddress string `json:"validator_address"`
 	Amount           string `json:"amount"`
 	ChainName        string `json:"chain_name"`
 }
 
-type unbondingDelegationsResponse struct {
-	UnbondingDelegations []unbondingDelegation `json:"unbonding_delegations"`
+type UnbondingDelegationsResponse struct {
+	UnbondingDelegations []UnbondingDelegation `json:"unbonding_delegations"`
 }
 
-type unbondingDelegation struct {
+type UnbondingDelegation struct {
 	ValidatorAddress string                                   `json:"validator_address"`
 	Entries          tracelistener.UnbondingDelegationEntries `json:"entries"`
 	ChainName        string                                   `json:"chain_name"`
 }
 
-type numbersResponse struct {
+type NumbersResponse struct {
 	Numbers []tracelistener.AuthRow `json:"numbers"`
 }
 
-type userTicketsResponse struct {
+type UserTicketsResponse struct {
 	Tickets map[string][]string `json:"tickets"`
 }
 
-type delegationDelegatorReward struct {
+type DelegationDelegatorReward struct {
 	ValidatorAddress string `json:"validator_address,omitempty"`
 	Reward           string `json:"reward"`
 }
 
-type delegatorRewardsResponse struct {
-	Rewards []delegationDelegatorReward `json:"rewards"`
+type DelegatorRewardsResponse struct {
+	Rewards []DelegationDelegatorReward `json:"rewards"`
 	Total   string                      `json:"total"`
 }
