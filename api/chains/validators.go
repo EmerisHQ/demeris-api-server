5cf0b9f779048318144b88aeeb64720c320dcbd0
Antonio Pitasi <antonio@pitasi.dev>
2022-02-15 18:17:45 +0100

    feat(chains): add avatar field to validator results
    Right now we fetch the avatar on-fly all the time, while we need to
    introduce a cache layer in order to not call keybase all the times.

--- api/chains/validators.go
+++ api/chains/validators.go
@@ -1,10 +1,13 @@
 package chains
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/allinbits/demeris-api-server/api/router/deps"
+	"github.com/allinbits/demeris-api-server/lib/keybase"
+	"github.com/allinbits/demeris-backend-models/tracelistener"
 	"github.com/gin-gonic/gin"
 )
 
@@ -69,7 +72,44 @@ func GetValidators(c *gin.Context) {
 		return
 	}
 
-	res.Validators = validators
+	adaptValidators := make([]*Validator, 0, len(validators))
+	for _, v := range validators {
+		adapted, err := adaptValidator(c.Request.Context(), v)
+		if err != nil {
+			d.Logger.Warnw(
+				"cannot get avatar for validator",
+				"validatorIdentity", v.Identity,
+				"error", err,
+			)
+		}
+
+		adaptValidators = append(adaptValidators, adapted)
+	}
+
+	res.Validators = adaptValidators
 
 	c.JSON(http.StatusOK, res)
+}
+
+func adaptValidator(c context.Context, r tracelistener.ValidatorRow) (*Validator, error) {
+	var v = &Validator{ValidatorRow: r}
+	var err error
+
+	if len(r.Identity) > 0 {
+		v.Avatar, err = getAvatar(c, r.Identity)
+	}
+
+	return v, err
+}
+
+func getAvatar(c context.Context, keySuffix string) (string, error) {
+	// TODO: introduce caching
+
+	avatar, err := keybase.GetPictureByKeySuffix(c, keySuffix)
+	if err != nil {
+		// an error fetching avatar shouldn't block execution, we'll just log it
+		err = fmt.Errorf("keybase api: %w", err)
+	}
+
+	return avatar, err
 }
