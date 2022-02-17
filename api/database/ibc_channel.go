296d1c2c82014d359854981b9a34c8c51ec0835f
SpideyPool192 <krishna192reddy@gmail.com>
2022-02-17 11:17:43 +0530

    refactoring

--- api/database/ibc_channel.go
+++ api/database/ibc_channel.go
@@ -7,7 +7,89 @@ import (
 func (d *Database) GetIbcChannelToChain(chain, channel, chainID string) (cns.IbcChannelsInfo, error) {
 	var c cns.IbcChannelsInfo
 
-	q := `
+	// q := `
+	// SELECT
+	// 	c1.chain_name AS chain_a_chain_name,
+	// 	c1.channel_id AS chain_a_channel_id,
+	// 	c1.counter_channel_id AS chain_a_counter_channel_id,
+	// 	c1.chain_id AS chain_a_chain_id,
+	// 	c2.chain_name AS chain_b_chain_name,
+	// 	c2.channel_id AS chain_b_channel_id,
+	// 	c2.counter_channel_id AS chain_b_counter_channel_id,
+	// 	c2.chain_id AS chain_b_chain_id
+	// FROM
+	// 	(
+	// 		SELECT
+	// 			tracelistener.channels.chain_name,
+	// 			tracelistener.channels.channel_id,
+	// 			tracelistener.channels.counter_channel_id,
+	// 			tracelistener.clients.chain_id
+	// 		FROM
+	// 			tracelistener.channels
+	// 			LEFT JOIN tracelistener.connections ON
+	// 					tracelistener.channels.hops[1]
+	// 					= tracelistener.connections.connection_id
+	// 			LEFT JOIN tracelistener.clients ON
+	// 					tracelistener.clients.client_id
+	// 					= tracelistener.connections.client_id
+	// 		WHERE
+	// 			tracelistener.connections.chain_name
+	// 			= tracelistener.channels.chain_name
+	// 			AND tracelistener.clients.chain_name
+	// 				= tracelistener.channels.chain_name
+	// 	)
+	// 		AS c1,
+	// 	(
+	// 		SELECT
+	// 			tracelistener.channels.chain_name,
+	// 			tracelistener.channels.channel_id,
+	// 			tracelistener.channels.counter_channel_id,
+	// 			tracelistener.clients.chain_id
+	// 		FROM
+	// 			tracelistener.channels
+	// 			LEFT JOIN tracelistener.connections ON
+	// 					tracelistener.channels.hops[1]
+	// 					= tracelistener.connections.connection_id
+	// 			LEFT JOIN tracelistener.clients ON
+	// 					tracelistener.clients.client_id
+	// 					= tracelistener.connections.client_id
+	// 		WHERE
+	// 			tracelistener.connections.chain_name
+	// 			= tracelistener.channels.chain_name
+	// 			AND tracelistener.clients.chain_name
+	// 				= tracelistener.channels.chain_name
+	// 	)
+	// 		AS c2
+	// WHERE
+	// 	c1.channel_id = c2.counter_channel_id
+	// 	AND c1.counter_channel_id = c2.channel_id
+	// 	AND c1.chain_name != c2.chain_name
+	// 	AND c1.chain_name = ?
+	// 	AND c1.channel_id = ?
+	// 	AND c2.chain_id = ?
+
+	// `
+	x := `SELECT
+	tracelistener.channels.chain_name,
+	tracelistener.channels.channel_id,
+	tracelistener.channels.counter_channel_id,
+	tracelistener.clients.chain_id
+FROM
+	tracelistener.channels
+	INNER JOIN tracelistener.connections ON
+			tracelistener.channels.hops[1]
+			= tracelistener.connections.connection_id
+		AND
+			tracelistener.connections.chain_name
+			= tracelistener.channels.chain_name
+	INNER JOIN tracelistener.clients ON
+			tracelistener.clients.client_id
+			= tracelistener.connections.client_id
+		AND
+		tracelistener.clients.chain_name
+		= tracelistener.channels.chain_name`
+
+	y := `
 	SELECT
 		c1.chain_name AS chain_a_chain_name,
 		c1.channel_id AS chain_a_channel_id,
@@ -19,57 +101,24 @@ func (d *Database) GetIbcChannelToChain(chain, channel, chainID string) (cns.Ibc
 		c2.chain_id AS chain_b_chain_id
 	FROM
 		(
-			SELECT
-				tracelistener.channels.chain_name,
-				tracelistener.channels.channel_id,
-				tracelistener.channels.counter_channel_id,
-				tracelistener.clients.chain_id
-			FROM
-				tracelistener.channels
-				LEFT JOIN tracelistener.connections ON
-						tracelistener.channels.hops[1]
-						= tracelistener.connections.connection_id
-				LEFT JOIN tracelistener.clients ON
-						tracelistener.clients.client_id
-						= tracelistener.connections.client_id
-			WHERE
-				tracelistener.connections.chain_name
-				= tracelistener.channels.chain_name
-				AND tracelistener.clients.chain_name
-					= tracelistener.channels.chain_name
-		)
-			AS c1,
+			` + x + `
+		) c1	
+			INNER	JOIN
 		(
-			SELECT
-				tracelistener.channels.chain_name,
-				tracelistener.channels.channel_id,
-				tracelistener.channels.counter_channel_id,
-				tracelistener.clients.chain_id
-			FROM
-				tracelistener.channels
-				LEFT JOIN tracelistener.connections ON
-						tracelistener.channels.hops[1]
-						= tracelistener.connections.connection_id
-				LEFT JOIN tracelistener.clients ON
-						tracelistener.clients.client_id
-						= tracelistener.connections.client_id
-			WHERE
-				tracelistener.connections.chain_name
-				= tracelistener.channels.chain_name
-				AND tracelistener.clients.chain_name
-					= tracelistener.channels.chain_name
-		)
-			AS c2
+			` + x + `
+		) c2
+		ON c1.channel_id = c2.counter_channel_id
+		AND c1.counter_channel_id = c2.channel_id	
+			
 	WHERE
-		c1.channel_id = c2.counter_channel_id
-		AND c1.counter_channel_id = c2.channel_id
 		AND c1.chain_name != c2.chain_name
 		AND c1.chain_name = ?
 		AND c1.channel_id = ?
 		AND c2.chain_id = ?
 
 	`
-	q = d.dbi.DB.Rebind(q)
+
+	q := d.dbi.DB.Rebind(y)
 
 	err := d.dbi.DB.Select(&c, q, chain, channel, chainID)
 	if err != nil {
