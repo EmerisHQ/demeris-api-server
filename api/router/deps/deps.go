0e702e427b1de126853b6edea278c6ef87b22c6c
Antonio Pitasi <antonio@pitasi.dev>
2022-04-11 12:07:24 +0200

    refactor: remove logger from Deps

--- api/router/deps/deps.go
+++ api/router/deps/deps.go
@@ -5,17 +5,13 @@ import (
 	kube "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/emerishq/demeris-api-server/api/database"
-	"github.com/emerishq/demeris-api-server/lib/apierrors"
 	"github.com/emerishq/demeris-api-server/lib/ginutils"
-	"github.com/emerishq/emeris-utils/logging"
 	"github.com/emerishq/emeris-utils/store"
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 )
 
 // Deps represents a set of objects useful during the lifecycle of REST endpoints.
 type Deps struct {
-	Logger           *zap.SugaredLogger
 	Database         *database.Database
 	Store            *store.Store
 	K8S              *kube.Client
@@ -25,37 +21,5 @@ type Deps struct {
 
 func GetDeps(c *gin.Context) *Deps {
 	deps := ginutils.GetValue[*Deps](c, "deps")
-
-	// override logger with the one from the gin.context
-	logger, err := logging.GetLoggerFromContext(c)
-	if err == nil {
-		deps.Logger = logger
-	} else {
-		deps.Logger.Warnw("couldn't get logger from context, using fallback", "error", err)
-	}
-
 	return deps
-}
-
-// WriteError adds an error to the gin context and logs it.
-func (d *Deps) WriteError(c *gin.Context, err *apierrors.Error, logMessage string, keyAndValues ...interface{}) {
-	_ = c.Error(err)
-
-	if keyAndValues != nil {
-		keyAndValues = append(keyAndValues, "error", err)
-		d.Logger.Errorw(
-			logMessage,
-			keyAndValues...,
-		)
-	}
-}
-
-// LogError is used to log errors internally while returning 200 in the response
-func (d *Deps) LogError(logMessage string, keyAndValues ...interface{}) {
-	if keyAndValues != nil {
-		d.Logger.Errorw(
-			logMessage,
-			keyAndValues...,
-		)
-	}
 }
--- api/router/router.go
+++ api/router/router.go
@@ -121,7 +121,6 @@ func (r *Router) catchPanicsFunc(c *gin.Context) {
 
 func (r *Router) decorateCtxWithDeps(c *gin.Context) {
 	c.Set("deps", &deps.Deps{
-		Logger:           r.l,
 		Database:         r.DB,
 		Store:            r.s,
 		KubeNamespace:    r.k8sNamespace,
@@ -145,8 +144,12 @@ func (r *Router) handleErrors(c *gin.Context) {
 	}
 
 	keysAndValues := append(err.LogKeysAndValues, "error", err)
-	d := deps.GetDeps(c)
-	d.Logger.Errorw(
+
+	logger, loggerErr := logging.GetLoggerFromContext(c)
+	if loggerErr != nil {
+		panic(fmt.Sprintf("gin context didn't contain logger: %s", loggerErr))
+	}
+	logger.Errorw(
 		err.Error(),
 		keysAndValues...,
 	)
