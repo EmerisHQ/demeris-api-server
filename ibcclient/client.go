9f4bbeb7a8009d3b3b87f1b2c94739d266ed9ae6
Brian Luk <brian6.dev@gmail.com>
2022-04-19 22:28:45 -0700

    add ibcChannelClientState fetcher

---
+++ ibcclient/client.go
@@ -0,0 +1,26 @@
+package ibcclient
+
+import (
+	"fmt"
+
+	"context"
+
+	ibcchannel "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	"google.golang.org/grpc"
+)
+
+var grpcport = 9090
+
+func IbcChannelClientState(chainName string, channelId, portId string) (*ibcchannel.QueryChannelClientStateResponse, error) {
+	grpcConn, err := grpc.Dial(fmt.Sprintf("%s:%d", chainName, grpcport), grpc.WithInsecure())
+	if err != nil {
+		return &ibcchannel.QueryChannelClientStateResponse{}, err
+	}
+
+	defer func() {
+		_ = grpcConn.Close()
+	}()
+
+	iq := ibcchannel.NewQueryClient(grpcConn)
+	return iq.ChannelClientState(context.Background(), &ibcchannel.QueryChannelClientStateRequest{})
+}
