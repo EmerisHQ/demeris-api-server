ffcea67e4b2a180ecb31d4992dbef44272a810d0
Brian Luk <brian6.dev@gmail.com>
2022-04-27 11:07:10 -0700

    add ErrNoMatchingChannels

--- api/database/errors.go
+++ api/database/errors.go
@@ -10,3 +10,9 @@ type ErrNoDestChain struct {
 func (e ErrNoDestChain) Error() string {
 	return fmt.Sprintf("no destination chain found for %s -> %s -> destination", e.Chain_a, e.Channel)
 }
+
+type ErrNoMatchingChannels struct{}
+
+func (e ErrNoMatchingChannels) Error() string {
+	return fmt.Sprintf("no matching channels found")
+}
