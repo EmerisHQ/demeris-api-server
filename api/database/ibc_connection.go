3fbf8d6afc56d68e60973a86d107a4a458456b38
akhilkumarpilli <akhilkumar7947@gmail.com>
2022-04-27 21:06:43 +0530

    Add few more tests

--- api/database/ibc_connection.go
+++ api/database/ibc_connection.go
@@ -1,9 +1,13 @@
 package database
 
-import "github.com/emerishq/demeris-backend-models/tracelistener"
+import (
+	"fmt"
+
+	"github.com/emerishq/demeris-backend-models/tracelistener"
+)
 
 func (d *Database) Connection(chain string, connection_id string) (tracelistener.IBCConnectionRow, error) {
-	var connection tracelistener.IBCConnectionRow
+	var connections []tracelistener.IBCConnectionRow
 
 	q := `
 	SELECT *
@@ -11,9 +15,18 @@ func (d *Database) Connection(chain string, connection_id string) (tracelistener
 	WHERE chain_name=?
 	AND connection_id=?
 	AND delete_height IS NULL
+	limit 1
 	`
 
 	q = d.dbi.DB.Rebind(q)
 
-	return connection, d.dbi.DB.Select(&connection, q, chain, connection_id)
+	if err := d.dbi.DB.Select(&connections, q, chain, connection_id); err != nil {
+		return tracelistener.IBCConnectionRow{}, err
+	}
+
+	if len(connections) == 0 {
+		return tracelistener.IBCConnectionRow{}, fmt.Errorf("query done but returned no result")
+	}
+
+	return connections[0], nil
 }
